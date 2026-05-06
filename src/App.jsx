ba905828712f0c01cccecddf287c51d0dee45f5c
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-05-06 14:54:10 +0200

    inizializzazione repo con array di oggetti

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+const initialMovies = [
+   { title: 'Inception', genre: 'Fantascienza' },
+   { title: 'Il Padrino', genre: 'Thriller' },
+   { title: 'Titanic', genre: 'Romantico' },
+   { title: 'Batman', genre: 'Azione' },
+   { title: 'Interstellar', genre: 'Fantascienza' },
+   { title: 'Pulp Fiction', genre: 'Thriller' },
+]
+
+
+function App() {
+  return <button className='btn btn-primary'>ciao</button>;
+}
+
+export default App
