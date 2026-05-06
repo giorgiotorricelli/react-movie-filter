9ed0454fbd58faae7ae1346a9d337c22692ca789
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-05-06 16:07:23 +0200

    utilizzato state e use effect con la select

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useEffect } from 'react';
 
 const initialMovies = [
    { title: 'Inception', genre: 'Fantascienza' },
@@ -6,12 +7,39 @@ const initialMovies = [
    { title: 'Titanic', genre: 'Romantico' },
    { title: 'Batman', genre: 'Azione' },
    { title: 'Interstellar', genre: 'Fantascienza' },
-   { title: 'Pulp Fiction', genre: 'Thriller' },
+   { title: 'Pulp Fiction', genre: 'Thriller' }
 ]
 
 
 function App() {
-  return <button className='btn btn-primary'>ciao</button>;
+  const [selectValue, setSelectValue] = useState('Fantascienza');
+  // const [movieGenre, setMovieGenre] = useState(selectValue)
+
+  useEffect(() => {
+    console.log(`scelto genere ${selectValue}`)  // setup callback
+    return () => {  // cleanup callback
+      console.log(`uscito dal genere ${selectValue}`)
+    } 
+  }, [selectValue]);
+
+  function selectChangeHandler(event){
+    const target = event.target;
+    const value = target.value;
+    setSelectValue(value);
+  }
+
+
+  return <>
+    <div className="main-wrapper d-flex justify-content-center">
+      <select value={selectValue} onChange={selectChangeHandler} name="genres" id="genres">
+        <option value="Fantascienza">Fantascienza</option>
+        <option value="Thriller">Thriller</option>
+        <option value="Romantico">Romantico</option>
+        <option value="Azione">Azione</option>
+      </select>
+      <p>{selectValue}</p>
+    </div>
+  </>;
 }
 
 export default App
